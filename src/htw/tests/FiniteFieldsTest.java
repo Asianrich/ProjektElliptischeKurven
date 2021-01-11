370cf70b058dddd8c64a388f8dbd326fbdd21aa5
Rich <manhkhang@gmx.de>
2021-01-11 13:38:27 +0100

    Primzahl in einer txt datei

--- src/htw/tests/FiniteFieldsTest.java
+++ src/htw/tests/FiniteFieldsTest.java
@@ -5,16 +5,30 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileWriter;
 import java.math.BigInteger;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 public class FiniteFieldsTest {
 
     @Test
-    public void FermatTest_4_bit(){
-        FiniteFields Field = new FiniteFields();
-        BigInteger prim = Field.generatePrime(4, 5);
+    public void FermatTest_4_bit() throws IOException {
+
+        FileWriter myFile = new FileWriter("%username%\\Desktop\\prim.txt");
+        for(int i = 0; i < 4; i++){
+            StringBuilder sb = new StringBuilder();
+            BigInteger prim = getPrim(4, 4);
+            sb.append("Primzahl ist ");
+            sb.append(prim);
+            sb.append("\n");
+            myFile.write(sb.toString());
+        }
+        myFile.close();
+
+
         Assert.assertTrue(true);
     }
     @Test
@@ -58,10 +72,26 @@ public class FiniteFieldsTest {
     }
 
     @Test
-    public void FermatTest_8192_bit(){
-        FiniteFields Field = new FiniteFields();
-        BigInteger prim = Field.generatePrime(8192, 4);
-        BigInteger test = prim;
+    public void FermatTest_8192_bit() throws IOException {
+        FileWriter myFile = new FileWriter("%username%\\Desktop\\prim.txt");
+
+
+        for(int i = 0; i < 4; i++){
+            StringBuilder sb = new StringBuilder();
+            BigInteger prim = getPrim(8192, 4);
+            sb.append("Primzahl ist ");
+            sb.append(prim);
+            sb.append("\n");
+            myFile.write(sb.toString());
+        }
+
+        myFile.close();
         Assert.assertTrue(true);
+    }
+
+    private BigInteger getPrim(int len, int trials){
+        FiniteFields Field = new FiniteFields();
+        BigInteger prim = Field.generatePrime(len, trials);
+        return prim;
     }
 }
